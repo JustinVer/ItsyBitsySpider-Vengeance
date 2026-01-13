d9378ad15115558c216d0e32d1f047ea6d0af0c5
LiamWellstood <Liam@wellstood.ca>
2026-01-13 11:25:53 -0500

    Bird Attack works

--- Assets/Scripts/Hazards/BirdAttack.cs
+++ Assets/Scripts/Hazards/BirdAttack.cs
@@ -4,21 +4,65 @@ using UnityEngine;
 public class BirdAttack : MonoBehaviour
 {
     [SerializeField] GameObject beak;
+    GameObject player;
+    Vector3 targetPosition;
+    [SerializeField] float attackDuration = 0.25f;
+    //number indicates what the bird should be doing
+    //0 = Idle, 1 = poking forward, 2 = retreating
+    private int attackStage = 0;
+    private Vector3 homePos;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        homePos = beak.transform.position;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider collision)
     {
-
+        if (attackStage == 0)
+        {
+            targetPosition = collision.transform.position;
+            attackStage = 1;
+            beak.transform.LookAt(collision.transform.position);
+            beak.transform.Rotate(90, 0, 0);
+            StartCoroutine(moveBeak(collision.transform.position, attackDuration));
+        }
     }
 
-    private void OnCollisionEnter(Collision collision)
+    IEnumerator moveBeak(Vector3 targetPosition, float duration)
     {
+        float timeMoving = 0;
+        Vector3 startPosition = beak.transform.position;
 
+        while (timeMoving < duration)
+        {
+            float t = timeMoving / duration;
+            t = Mathf.SmoothStep(0f, 1f, t);
+
+
+            beak.transform.position = Vector3.Lerp(startPosition, targetPosition, t);
+
+            timeMoving += Time.deltaTime;
+
+            yield return null;
+        }
+
+        // Ensure the object reaches the exact final position
+        transform.position = targetPosition;
+        returnToStart();
+    }
+
+    private void returnToStart()
+    {
+        if (attackStage == 1)
+        {
+            attackStage = 2;
+            StartCoroutine(moveBeak(homePos, attackDuration * 4));
+        }
+        else
+        {
+            attackStage = 0;
+        }
     }
 }
