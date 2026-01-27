37e6511a450a8d1d4cd1590e787c3c3c2cce377b
JustinVer <pokemasterjtv@gmail.com>
2026-01-27 14:19:53 -0500

    Another small change to the gun ad moved the scripts from the test folder

--- Assets/Scripts/Player/Gun.cs
+++ Assets/Scripts/Player/Gun.cs
@@ -12,19 +12,28 @@ public class Gun : MonoBehaviour
 
     [SerializeField] float rotationSpeed = 180f;
 
-    [SerializeField] Vector3 hitPosition;
+    [SerializeField] Transform folloeTransform;
+
+    Vector3 hitPosition;
 
     // Update is called once per frame
     void Update()
     {
+        this.transform.position = folloeTransform.position;
         //transform.LookAt(camPoint.position + camPoint.forward * 100f);
         Ray ray = playerCamera.ScreenPointToRay(new Vector3(Screen.width / 2f, Screen.height / 2f, 0));
         RaycastHit hit;
-        Physics.Raycast(ray, out hit, 999f, GameplayManager.Instance.NotPlayerOrEnemyMask, QueryTriggerInteraction.Ignore);
+        if (Physics.Raycast(ray, out hit, 200f, GameplayManager.Instance.NotPlayerOrEnemyMask, QueryTriggerInteraction.Ignore))
+        {
+            hitPosition = hit.point;
+        }
+        else
+        {
+            hitPosition = playerCamera.transform.position + playerCamera.transform.forward * 200f;
+        }
 
-        hitPosition = hit.point;
 
-        this.transform.rotation = Quaternion.RotateTowards(this.transform.rotation, Quaternion.LookRotation(hit.point - this.transform.position, GameplayManager.Instance.GetGravity(this.transform.position)), rotationSpeed * Time.deltaTime);
+        this.transform.rotation = Quaternion.RotateTowards(this.transform.rotation, Quaternion.LookRotation(hitPosition - this.transform.position, GameplayManager.Instance.GetGravity(this.transform.position)), rotationSpeed * Time.deltaTime);
         //Debug.DrawRay(ray.origin, ray.direction * 10, Color.yellow, 1f);
         //Debug.DrawLine(this.transform.position, hit.point, Color.red, 1f);
 
