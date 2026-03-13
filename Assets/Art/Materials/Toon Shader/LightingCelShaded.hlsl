8992963062ea860d56d8bde5ee196548a8d66c9b
ParkerRailton <parker.railton@gmail.com>
2026-03-13 11:06:42 -0400

    porting cel shader to Forward+.

--- Assets/Art/Materials/Toon Shader/LightingCelShaded.hlsl
+++ Assets/Art/Materials/Toon Shader/LightingCelShaded.hlsl
@@ -1,6 +1,12 @@
 ﻿#ifndef LIGHTING_CEL_SHADED_INCLUDED
 #define LIGHTING_CEL_SHADED_INCLUDED
 
+#pragma multi_compile _ _FORWARD_PLUS
+#pragma multi_compile_fragment _ _ADDITIONAL_LIGHTS
+#pragma multi_compile _ _ADDITIONAL_LIGHT_SHADOWS
+
+#include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Lighting.hlsl" 
+
 #ifndef SHADERGRAPH_PREVIEW
 struct EdgeConstants {
 
@@ -72,7 +78,7 @@ void LightingCelShaded_float(float Smoothness,
    s.rimStrength = RimStrength;
    s.rimAmount = RimAmount;
    s.rimThreshold = RimThreshold;
-   s.normal = normalize(Normal);
+    s.normal = SafeNormalize(Normal);
    s.view = SafeNormalize(View);
    s.ec.diffuse = EdgeDiffuse;
    s.ec.specular = EdgeSpecular;
@@ -86,15 +92,30 @@ void LightingCelShaded_float(float Smoothness,
 #else
    float4 shadowCoord = TransformWorldToShadowCoord(Position);
 #endif
+    
+    
+    Color = 0;
+    
+    Light light = GetMainLight(shadowCoord);
+    Color = CalculateCelShading(light, s);
 
-   Light light = GetMainLight(shadowCoord);
-   Color = CalculateCelShading(light, s);
+    
 
-   int pixelLightCount = GetAdditionalLightsCount();
-   for (int i = 0; i < pixelLightCount; i++) {
-      light = GetAdditionalLight(i, Position, 1);
-      Color += CalculateCelShading(light, s);
-   }
+#if defined(_ADDITIONAL_LIGHTS)
+
+uint pixelLightCount = GetAdditionalLightsCount();
+  
+InputData inputData = (InputData)0;
+inputData.positionWS = Position;
+inputData.normalWS = s.normal;
+inputData.viewDirectionWS = s.view;
+    
+LIGHT_LOOP_BEGIN(pixelLightCount)
+    Light addLight = GetAdditionalLight(lightIndex, Position);
+    Color += CalculateCelShading(addLight, s);
+LIGHT_LOOP_END
+
+#endif
    
 #endif
 }
